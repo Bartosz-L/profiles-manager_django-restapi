049ab6e334c4f588c9e0c36caac28348011119fd
Bartosz Ligęza <bligeza@tidk.pl>
2019-04-30 16:00:09 +0200

    created login api viewset

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -81,3 +83,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class LoginViewSet(viewsets.ViewSet):
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        return ObtainAuthToken().post(request)
