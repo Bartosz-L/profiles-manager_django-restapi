fc29801ce399601fd8f8f2362bb4287afafb4800
Bartosz Ligęza <bligeza@tidk.pl>
2019-04-30 15:53:17 +0200

    added filtering users in api by name or email

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -78,3 +78,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
